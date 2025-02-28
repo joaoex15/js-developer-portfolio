5c2cba6f8df9813a28d48f43dbb440a94dfea037
joaoex15 <joaosantoosrocha@gmail.com>
2025-02-27 22:00:13 -0300

    Atualização de foto

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://joaoex15.github.io/js-developer-portfolio/profile.json';
+    const url = 'https://joaoex15.github.io/js-developer-portfolio/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
